52eef2d287d7abd1f2299355867098e1779a6515
semlevin <semenslevin@gmail.com>
2019-05-29 19:00:00 +0300

    try6

--- con2.cpp
+++ con2.cpp
@@ -6,14 +6,65 @@
 int main()
 {
 	int arg1;
-	char bin[6];
+	char bin[10];
 
 again:
 
 	printf(" hex ");
 	scanf_s ("%x", &arg1);
 
-	switch (arg1)
+	// 7
+	if (arg1 & 0x80)
+		bin[0] = '1';
+	else
+		bin[0] = '0';
+
+	// 6
+	if (arg1 & 0x40)
+		bin[1] = '1';
+	else
+		bin[1] = '0';
+
+	// 5
+	if (arg1 & 0x20)
+		bin[2] = '1';
+	else
+		bin[2] = '0';
+
+	// 4
+	if (arg1 & 0x10)
+		bin[3] = '1';
+	else
+		bin[3] = '0';
+
+	// 3
+	if (arg1 & 0x08)
+		bin[4] = '1';
+	else
+		bin[4] = '0';
+
+	// 2
+	if (arg1 & 0x04)
+		bin[5] = '1';
+	else
+		bin[5] = '0';
+
+	// 1
+	if (arg1 & 0x02)
+		bin[6] = '1';
+	else
+		bin[6] = '0';
+
+	// 0
+	if (arg1 & 0x01)
+		bin[7] = '1';
+	else
+		bin[7] = '0';
+
+	bin[8] = 'b';
+	bin[9] = 0;
+
+	/*switch (arg1)
 	{
 	case 0:
 		strcpy_s(bin, "0000b");
@@ -78,11 +129,13 @@ again:
 	case 0x0F:
 		strcpy_s(bin, "1111");
 		break;
-	}
-	printf("\n%s", bin);
+	}*/
+
+	printf("\n%s\n", bin);
 	
 	if (arg1 == 0x0A)
 		return 0;
+
 	goto again;
 
 }

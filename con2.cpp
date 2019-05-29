8a93e0400583c185b4edcd4abee4f2a84c894497
semlevin <semenslevin@gmail.com>
2019-05-29 18:23:32 +0300

    try5

--- con2.cpp
+++ con2.cpp
@@ -1,84 +1,87 @@
-// con2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 #define use_CRT_SECURE_NO_WARNIGNS
 #include <iostream>
+
 
 int main()
 {
 	int arg1;
 	char bin[6];
-
+	
+	printf(" hex ");
+	scanf_s ("%x", &arg1);
+	
 
 
 
 	switch (arg1)
 	{
 	case 0:
-		(bin, "0000b");
+		strcpy_s(bin, "0000b");
 		break;
 
 	case 1:
-		(bin, "0001b");
+		strcpy_s(bin, "0001b");
 		break;
 
 	case 2:
-		(bin, "0010b");
+		strcpy_s(bin, "0010b");
 		break;
 
 	case 3:
-		(bin, "0011b");
+		strcpy_s(bin, "0011b");
 		break;
 
 	case 4:
-		(bin, "0100b");
+		strcpy_s(bin, "0100b");
 		break;
 	
 	case 5:
-		(bin, "0101");
+		strcpy_s(bin, "0101");
 		break;
 
 	case 6:
-		(bin, "0110");
+		strcpy_s(bin, "0110");
 		break;
 
 	case 7:
-		(bin, "0111");
+		strcpy_s(bin, "0111");
 		break;
 
 	case 8:
-		(bin, "1000");
+		strcpy_s(bin, "1000");
 		break;
 
 	case 9:
-		(bin, "1000");
+		strcpy_s(bin, "1000");
 		break;
 
 	case 0x0A:
-		(bin, "1010");
+		strcpy_s(bin, "1010");
 		break;
 
 	case 0x0B:
-		(bin, "1011");
+		strcpy_s(bin, "1011");
 		break;
 
 	case 0x0C:
-		(bin, "1100");
+		strcpy_s(bin, "1100");
 		break;
 
 	case 0x0D:
-		(bin, "1101");
+		strcpy_s(bin, "1101");
 		break;
 
 	case 0x0E:
-		(bin, "1110");
+		strcpy_s(bin, "1110");
 		break;
 
 	case 0x0F:
-		(bin, "1111");
+		strcpy_s(bin, "1111");
 		break;
 	}
-
-
+	printf("\n%s", bin);
+	
 
 
 }

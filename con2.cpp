e49a6417f0e0b37f19f1a9d5644a3852d199e445
semlevin <49359054+semlevin@users.noreply.github.com>
2019-05-29 15:39:23 +0300

    try2

---
+++ con2.cpp
@@ -0,0 +1,41 @@
+// con2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#define use_CRT_SECURE_NO_WARNIGNS
+#include <iostream>
+
+int main()
+{
+	int arg1;
+	char bin[6];
+
+
+
+
+	switch (arg1)
+	{
+		case 0:
+			(bin, "0000");
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+}
